5f7f9f796729c4b55b0f5ab21fc654aff9ff60e0
nashvador <nashvador@gmail.com>
2022-12-16 01:17:58 -0500

    Updated TwoSum solution

--- TwoSum.js
+++ TwoSum.js
@@ -3,12 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
+var twoSum = function(nums, target) {
+    const arraySet = new Map();
     for (let j = 0; j < nums.length; j++) {
-        for (let i = j + 1; i < nums.length; i++) {
-            if (nums[j] + nums[i] === target) {
-                return [j, i]
-            }
-        }    
+        // for (let i = j + 1; i < nums.length; i++) {
+        //     if (nums[j] + nums[i] === target) {
+        //         return [j, i]
+        //     }
+        // }   
+        const val = target - nums[j];
+        if (arraySet.has(val)) {
+            return [j, arraySet.get(val)]
+        }
+        arraySet.set(nums[j], j);
     }
 };
